b6a4ac04e39e99c6fc33b63267e38318b0d580ae
kaiseng <tankaiseng98@hotmail.com>
2018-10-01 14:46:58 +0800

    third commit

--- popup/DownloadItem.js
+++ popup/DownloadItem.js
@@ -1,3 +1,33 @@
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var suffix = "AM";
+  if (h >= 12) {
+  suffix = "PM";
+  h = h - 12;
+  }
+  if (h == 0) {
+  h = 12;
+  }
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s + " " + suffix;
+  t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
+startTime();
+
 var latestDownloadId;
 
 /*
